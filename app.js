cce512e57b0aa27d416a2ce2c67a111a0395e1ed
Shaun Donnelly <shaundonnelly@gmail.com>
2015-06-11 10:04:58 +0100

    - Removed cache from git, should be ignored. - Changed app to use skimdb.npmjs.com instead of registry.

--- app.js
+++ app.js
@@ -2,46 +2,78 @@ var npm = require('npm');
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var http = require('http').createServer(app);
+var https = require('https');
+
 app.set('port', (process.env.PORT || 5000));
 
 var SEARCH_TERMS = 'hubot-scripts';
 var RESULT_FILENAME = 'public/cache.json';
+var PACKAGES_LOCATION = 'https://skimdb.npmjs.com/registry/_design/app/_view/byKeyword?startkey=[%22' + SEARCH_TERMS + '%22]&endkey=[%22' + SEARCH_TERMS + '%22,{}]&group_level=3';
 var REFRESH_FREQUENCY = 14400000; // 4 hours.
 
 function getScripts(callback) {
 	console.log('Getting available scripts..');
-	npm.load({}, function() {
-		npm.commands.search(SEARCH_TERMS, true, 0, function(err, data) {
-			if (err) {
-				console.log('Error!', err);
-				return;
-			}
-			console.log('Retrieved packages.');
-			callback(data);
+
+	var request = https.get(PACKAGES_LOCATION, function(response) {
+
+		var completeResponse = "";
+
+		console.log('Response incoming..');
+		response.on('data', function(chunk) {
+			completeResponse += chunk;
 		});
+		response.on('end', function() {
+			callback(completeResponse);
+		});		
 	});
 }
 
 function saveResultsToFile(data) {
-	console.log('Saving results to file..');
 
-	fs.writeFile(RESULT_FILENAME, JSON.stringify(sanitiseResults(data)), function(err) {
+	fs.writeFile(RESULT_FILENAME, sanitiseResults(data), function(err) {
 		if (err) {
 			console.log(err);
 		}
+		console.log('Package list updated. See ' + RESULT_FILENAME);
 	});
 }
 
 function sanitiseResults(data) {
 
-	// Add a 'meta' section to the data.
-	return {
+	/*
+	Takes the response, does some processing on it and adds a 'meta'
+	object to store additional info.
+	*/
+
+	try {
+		data = JSON.parse(data);
+	}
+	catch (ex) {
+		console.log("Could not parse response into JSON.", ex);
+		return "";
+	}
+
+	var sanitised = {
 		meta: {
-			lastRefreshed: new Date(),
+			lastRefreshed: new Date()
 		},
-		packages: data
+		packages: []
 	};
+
+	if (data.rows) {
+		for (var i in data.rows) {
+			var package = data.rows[i];
+			if (package.key) {
+				var sanitisedPackage = {
+					name: package.key[1] || '',
+					description: package.key[2] || ''
+				}
+				sanitised.packages.push(sanitisedPackage);
+			}
+		}
+	}
+
+	return JSON.stringify(sanitised);
 }
 
 // On load, refresh scripts.
